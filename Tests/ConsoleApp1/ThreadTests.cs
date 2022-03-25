970af8d8bbd5fbbe7da7b66343e9407c0b2e93cc
Vladimir <shmalko89@list.ru>
2022-03-25 14:17:34 +0400

    Прерывание потока штатными методами

--- Tests/ConsoleApp1/ThreadTests.cs
+++ Tests/ConsoleApp1/ThreadTests.cs
@@ -16,7 +16,7 @@ namespace ConsoleApp1
 
             Console.ReadLine();
 
-
+            _UpdateTitleTimer = false;
             if (!timer_thread.Join(300))
             {
                 timer_thread.Interrupt();
@@ -27,16 +27,21 @@ namespace ConsoleApp1
             Console.WriteLine("Программа завершена!");
         }
 
+        private static bool _UpdateTitleTimer = true;
+
         private static void TimerUpdate()
         {
             try
             {
-                while (true)
+                while (_UpdateTitleTimer)
                 {
                     Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm.ss.ffff");
 
                     Thread.Sleep(250);
                 }
+
+                Console.WriteLine("Метод обновления часов завершен штатно");
+
             }
             catch (ThreadInterruptedException)
             {
