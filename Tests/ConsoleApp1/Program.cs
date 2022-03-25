9cd56e6f28563b7cac20e6ed15e6d0b09b5de933
Vladimir <shmalko89@list.ru>
2022-03-25 13:48:20 +0400

    Рефакторинг

--- Tests/ConsoleApp1/Program.cs
+++ Tests/ConsoleApp1/Program.cs
@@ -1,19 +1,8 @@
 ﻿using System.Threading;
+using ConsoleApp1;
 
 //var current_thread = Thread.CurrentThread;
 
 //current_thread.Priority = ThreadPriority.Lowest;
 
-while (true)
-{
-    Console.Title = DateTime.Now.ToString("dd.MM.yyyy HH:mm.ss.ffff");
-
-    Thread.Sleep(250);
-}
-
-
-Console.WriteLine("Программа ожидает ввода пользователя");
-
-Console.ReadLine();
-
-Console.WriteLine("Программа завершена!");
+ThreadTests.Run();
