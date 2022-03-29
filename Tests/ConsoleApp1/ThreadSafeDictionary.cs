c9e8678748ff5259d993e23a5b36ae4e386b79e3
Vladimir <shmalko89@list.ru>
2022-03-29 15:56:24 +0400

    оптимизация кода

--- Tests/ConsoleApp1/ThreadSafeDictionary.cs
+++ Tests/ConsoleApp1/ThreadSafeDictionary.cs
@@ -14,16 +14,19 @@ internal static class ThreadSafeDictionary
     {
         var strings = Enumerable.Range(1, 1000).Select(i => $"String -{i % 54}");
 
-        var hash_pool = new ConcurrentDictionary<string, byte[]>();
         foreach (var str in strings)
             ThreadPool.QueueUserWorkItem(_ =>
             {
-                var hash = hash_pool.GetOrAdd(str, s => ComputeHash(s));
+                var hash = ComputeHashBuffered(str);
                 Console.WriteLine("{0} : {1}", str, Convert.ToBase64String(hash));
             });
 
         Console.ReadLine();
     }
+
+    private static readonly ConcurrentDictionary<string, byte[]> __HashBuffer = new();
+
+    private static byte[] ComputeHashBuffered(string str) => __HashBuffer.GetOrAdd(str, s => ComputeHash(s));
 
     private static byte[] ComputeHash(string str)
     {
