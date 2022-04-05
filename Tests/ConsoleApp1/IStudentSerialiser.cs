294b8138753987c80f26709d0c36fa972b2bb281
Vladimir <shmalko89@list.ru>
2022-04-05 21:56:19 +0400

    рефакторинг

---
+++ Tests/ConsoleApp1/IStudentSerialiser.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1;
+
+public interface IStudentSerialiser
+{
+    void Serialise(TextWriter writer, List<Student> students);
+
+    List<Student> DeSerialize(TextReader reader);
+
+
+}
--- Tests/ConsoleApp1/Program.cs
+++ Tests/ConsoleApp1/Program.cs
@@ -2,41 +2,12 @@
 using System.Xml.Serialization;
 
 
-var student = new Student
-{
-    Id = 1,
-    FirstName = "Ivan",
-    LastName = "Ivanov",
-    Patronymic = "Ivanovich",
-    Birtday = DateTime.Now.AddYears(-18)
-    
-};
+var student_manager = new StudentManager();
 
-var xml_sirialiser = new XmlSerializer(typeof(Student));
+student_manager.Add("Иванов", "Иван", "Иванович", DateTime.Now.AddYears(-18));
+student_manager.Add("Петров", "Петр", "Петрович", DateTime.Now.AddYears(-36));
+student_manager.Add("Дмитриев", "Дмитрий", "Дмитриевич", DateTime.Now.AddYears(-25));
 
-const string xml_file_name = "student.xml";
-
-using (var xml_file = File.CreateText(xml_file_name))
-{
-    xml_sirialiser.Serialize(xml_file, student);
-
-};
-// using тоже самое что 
-
-/*var xml_file = File.CreateText(xml_file_name);
-try
-{
-    xml_sirialiser.Serialize(xml_file, student);
-}
-finally
-{
-    xml_file.Dispose();
-}*/
-
-Student? student2 = null;
-using (var xml_file = File.OpenText(xml_file_name))
-{
-    student2 = (Student?)xml_sirialiser.Deserialize(xml_file);
-};
-
-    Console.ReadLine();
+const string file_name = "students.xml";
+student_manager.SaveTo(file_name);
+Console.ReadLine();
