99d8842beba89137fbbb850903c3c33159ad1202
Vladimir <shmalko89@list.ru>
2022-04-09 15:11:28 +0400

    Про делегаты и функц программирование

---
+++ Tests/ConsoleApp1/LambdaExpressoins.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1;
+
+internal class LambdaExpressoins
+{
+    public static void Run()
+    {
+        string str = "123";
+        int x = 42;
+        List<int> ints = new();
+        Student student = new();
+        Process(student);
+
+        StudentProcessor process = Free;
+
+        ProcessStudent(Enumerable.Empty<Student>(), Process);
+
+        Action<Student> processor2 = Free;
+        Func<Student, int> metric = s => s.LastName.Length;
+
+        Func<double, double> sin = x =>Math.Sin(x);
+        Func<double, double> cos = x => Math.Cos(x);
+
+        Func<double, double> sum_sin_cos = Sum(sin, cos);
+
+        var resulr = sum_sin_cos(Math.PI / 3);
+    }
+
+    public static Func<double, double> Sum (Func<double, double> a, Func<double, double> b)
+    {
+        return x => a(x) + b(x);
+    }
+
+    public delegate void StudentProcessor(Student student);
+
+    public static void ProcessStudent (IEnumerable<Student> student, StudentProcessor processor /*делегат*/)
+    {
+        foreach(var std in student)
+            processor(std);
+    }
+
+    public static void Process(Student student)
+    {
+        Console.WriteLine(student);
+    }
+
+    public static void Free (Student student)
+    {
+        Console.WriteLine(student);
+    }
+}
--- Tests/ConsoleApp1/Program.cs
+++ Tests/ConsoleApp1/Program.cs
@@ -1,13 +1,5 @@
 ﻿using ConsoleApp1;
 using System.Xml.Serialization;
 
-
-var student_manager = new StudentManager();
-
-student_manager.Add("Иванов", "Иван", "Иванович", DateTime.Now.AddYears(-18));
-student_manager.Add("Петров", "Петр", "Петрович", DateTime.Now.AddYears(-36));
-student_manager.Add("Дмитриев", "Дмитрий", "Дмитриевич", DateTime.Now.AddYears(-25));
-
-const string file_name = "students.xml";
-student_manager.SaveTo(file_name);
+LambdaExpressoins.Run();
 Console.ReadLine();
